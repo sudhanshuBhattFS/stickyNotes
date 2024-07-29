ad8dbaec2a4fd17bcbd98e6cc4e00fceb3d84434
sudhanshuBhattFS <sudhanshu.bhatt@frootystudios.com>
2024-07-29 13:07:46 +0530

    Popup window will have a button to display new tab, where all website notes list will be visible

--- stickyNotes/stickyNotes.js
+++ stickyNotes/stickyNotes.js
@@ -15,13 +15,14 @@ document.addEventListener('DOMContentLoaded', function () {
     let isCardCreated = false
     let isNoteCreated = false
 
-    // first get the tab url
+    // query
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         hostName = new URL(tabs[0].url).hostname;
         url = tabs[0].url;
         console.log(url, 'url ')
     });
 
+    // inject function
     function injectPopUps(note) {
         console.log(note, hostName, "hostName and Note")
         chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
@@ -37,23 +38,27 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
-    // funtions
+    // retriveData 
     const retriveData = () => {
         chrome.storage.local.get('notes', function (result) {
             if (result.notes) {
                 noteArr = result.notes;
                 console.log(noteArr, 'note array ');
                 noteArr.forEach(element => {
-                    // inject card into the extension popup
-                    injectCards(element);
-                    // inject the note into the html if there is already stored value 
-                    injectPopUps(element)
+                    console.log(element, 'element')
+                    if (element.hostName === hostName) {
+
+                        // inject card into the extension popup
+                        injectCards(element);
+                        // inject the note into the html if there is already stored value 
+                        injectPopUps(element)
+                    }
                 });
             }
         });
     }
 
-
+    // createCard 
     const createCard = () => {
         //  store the details into localstorage 
         const noteHeading = getDateAndTime()
@@ -80,17 +85,20 @@ document.addEventListener('DOMContentLoaded', function () {
 
         const hostName = note.hostName
         const dateStr = note.date
-        const timeStr = note.time
-        const url = note.url
         const content = note.content
+        const url = note.url
+        const timeStr = note.time
+        const id = `${note.hostName}-${note.date.replace(/\//g, '-')}-${note.time.replace(/:/g, '-').replace(' PM', '-PM').replace(' AM', '-AM')}`;
 
         card.innerHTML = `
-                <div data-url="${url}" class="note-header url">
-                    <span>${hostName}</span>
+        <div>
+                <div class="note-header ">
+                    <span class="url" data-url="${url}">${hostName}</span>
                     <span> ${dateStr}</span>
-                    <span> ${timeStr}</span>
+                    <span id="${id}" class='delete-btn'> Delete </span>
                 </div>
-                <div contenteditable="false" style="overflow : hidden" >${content}</div>
+                <div contenteditable="false" style="overflow : hidden ;  height: 1rem;" >${content}</div>
+        </div>
             `;
 
         allListContainer.prepend(card);
@@ -98,11 +106,43 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // Add event listener to url span
         const urlSpan = card.querySelector('.url');
-        urlSpan.addEventListener('click', () => {
+        urlSpan.addEventListener('click', (event) => {
             chrome.tabs.create({ url: urlSpan.dataset.url }, (tab) => {
-                console.log(tab, 'check if this code works ')
+                console.log(tab, 'check if this code works');
             });
         });
+
+        // event listner for delete btn 
+        const deleteBtn = card.querySelector('.delete-btn');
+        deleteBtn.addEventListener('click', async () => {
+            if (confirm("Are you sure you want to Remove this Note")) {
+
+                card.remove();
+
+                const noteArr = await UserLocalStorage.retriveNoteData()
+                console.log(noteArr)
+                console.log(deleteBtn.id, 'id')
+                if (noteArr.length > 0) {
+                    noteArr.forEach((note) => {
+                        const id = `${note.hostName}-${note.date.replace(/\//g, '-')}-${note.time.replace(/:/g, '-').replace(' PM', '-PM').replace(' AM', '-AM')}`;
+                        console.log(id, 'id2')
+                        if (deleteBtn.id == id) {
+                            console.log(note, 'inside conidtion check ')
+                            // Remove the note from the array
+                            const newArr = noteArr.filter((n) => n !== note);
+                            console.log(newArr, 'new array ')
+                            UserLocalStorage.setStorage(newArr)
+
+                        }
+                    })
+                }
+
+                console.log("Note removed");
+            } else {
+                console.log("Note not removed");
+            }
+        });
+
 
     }
 
@@ -129,12 +169,10 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
-    chrome.runtime.onMessage.addListener((request) => {
-        console.log(request);
-        if (request.action === "") {
-            console.log('message is received')
-            createCard()
-        }
+
+    document.getElementById('openTabButton').addEventListener('click', () => {
+        chrome.runtime.sendMessage({ action: 'createTabAndInject' });
+
     });
 
 });
